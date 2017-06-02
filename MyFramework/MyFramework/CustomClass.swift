d21c3de53ec0f777a5ec074b60d5868e914e1e92
Matheus Gomes <omatheus015@gmail.com>
2017-06-02 17:17:52 -0300

    Framework adicionada

---
+++ MyFramework/MyFramework/CustomClass.swift
@@ -0,0 +1,40 @@
+//
+//  CustomClass.swift
+//  MyFramework
+//
+//  Created by Matheus Gomes Queiroz on 02/06/17.
+//  Copyright © 2017 BEPiD. All rights reserved.
+//
+
+import Foundation
+
+public class CustomClass: NSObject {
+    var blurEffectView: UIVisualEffectView = UIVisualEffectView()
+    
+    public func addBlurEffectIn(image: UIImageView) {
+        if image.image != nil {
+            if !blurEffectView.isDescendant(of: image) {
+                blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .regular))
+                blurEffectView.frame = image.frame
+                //blurEffectView.center = image.center
+                blurEffectView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+                
+                image.addSubview(blurEffectView)
+                print("Effect add!")
+                
+            } else {
+                print("Effect already added!")
+            }
+        } else {
+            print("No image!")
+        }
+    }
+    
+    public func removeBlurEffectIn(image: UIImageView) {
+        if blurEffectView.isDescendant(of: image) {
+            blurEffectView.removeFromSuperview()
+        } else {
+            print("Image without effect!")
+        }
+    }
+}
